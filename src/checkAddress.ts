2c6e45d2417eabadab2f812a293306533fb7ce76
mdasifahamed <mdasifahamed89@yahoo.com>
2024-11-13 12:10:18 +0600

    Validated Transaction Status And Check For Intrested Address

---
+++ src/checkAddress.ts
@@ -0,0 +1,17 @@
+import { TransactionReceipt } from "ethers";
+
+/**
+ * checkAddress()
+ * It checks for the status of the transaction and 
+ * Then checks, is the transaction is related to our wallet or not
+ * @param transactionReceipt the trasactionReciept object we are will be looking
+ * @param address address of the wallet
+ * @returns true/false
+ */
+export default function checkAddress(transactionReceipt:TransactionReceipt|null,address:string):boolean {
+    if(transactionReceipt?.status===1&&((transactionReceipt.from===address)||(transactionReceipt.to===address))){
+        return true;
+    }else{
+        return false;
+    }
+}

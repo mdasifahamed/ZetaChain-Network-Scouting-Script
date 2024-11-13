1e885643c57dc8224d3857b2385af82b3211ea0d
mdasifahamed <mdasifahamed89@yahoo.com>
2024-11-13 12:27:22 +0600

    Logs Transaction Receipt By Filtering It From List of Transactions of  A Block For An Interested Address

---
+++ src/logFilteredTransaction.ts
@@ -0,0 +1,31 @@
+import { TransactionReceipt,ethers } from "ethers";
+import { DataHexString } from "ethers/lib.commonjs/utils/data";
+import checkAddress from "./checkAddress"
+
+/**
+ * logFilteredTransaction()
+ * Logs The Filtered transaction from the list of the transactions hash
+ * by retriving the transaction receipt from onchain. 
+ * @param transactionHashes array of the transaction hash of a block
+ * @param provider node provider
+ * @param address intrested address for logging transaction
+ */
+
+export default async function logFilteredTransaction(transactionHashes:readonly DataHexString[] | undefined,provider:ethers.JsonRpcProvider,address:string) {
+    transactionHashes?.forEach(async(hash) => {
+        try {
+        const trxReceipt:TransactionReceipt|null=await provider.getTransactionReceipt(hash)
+            if(checkAddress(trxReceipt,address)){
+                console.log(`''''''''''' Loggin Started '''''''''''`)
+                console.log(trxReceipt)
+                console.log(`''''''''''' Loggin Started '''''''''''\n`)
+            }
+        } catch (error) {
+            console.log("Unexpected Error Occured\n")
+            console.log(error)
+        }
+   
+    
+    });
+    
+}

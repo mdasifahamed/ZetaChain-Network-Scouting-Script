b8a0ae9753d008eca490b32ee3ee0933e08604c1
mdasifahamed <mdasifahamed89@yahoo.com>
2024-11-15 18:12:03 +0600

    refactor
    refactored at return value, validiation included at provider and blocknumber, returned error to catch it in retry section

--- src/getTransactionList.ts
+++ src/getTransactionList.ts
@@ -1,20 +1,29 @@
-import { Block, ethers, Provider } from "ethers";
+import { Block, Provider } from "ethers";
 import { DataHexString } from 'ethers/lib.commonjs/utils/data';
 
-export default async function getTransactiList(blockNumber:number|undefined,provider:Provider):Promise< readonly DataHexString[]| undefined> {
+export default async function getTransactiList(
+    blockNumber: number | undefined,
+    provider: Provider | undefined
+): Promise<readonly DataHexString[]> {
 
-    if(blockNumber){
-        console.log(`Pulling Transaction list from the BlockNumber : ${blockNumber}`)
-        try {
-            const blockDetails:Block|null = await provider.getBlock(blockNumber)
-            if (blockDetails) {
-                const transactionHashList: readonly DataHexString[] = blockDetails.transactions
-                return transactionHashList;
-            }
-        } catch (error) {
-            console.log(`Unexpected Error Occured At Blokc:${blockNumber} While Pullling Transaction List`)
-            console.log(error)
-            return undefined          
+    if (!blockNumber || !provider) {
+        throw new Error("Block number or provider is undefined");
+    }
+
+    console.log(`Pulling Transaction list from the BlockNumber : ${blockNumber}`);
+
+    try {
+        const blockDetails = await provider.getBlock(blockNumber);
+
+        if (!blockDetails) {
+            throw new Error(`No block details found for block ${blockNumber}`);
         }
+
+        return blockDetails.transactions;
+
+    } catch (error) {
+        console.log(`Unexpected Error Occurred At Block:${blockNumber} While Pulling Transaction List`);
+        console.log(error);
+        throw error;
     }
 }
